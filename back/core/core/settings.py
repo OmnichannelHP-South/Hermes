fe559d5f7fcbe81f126a8688d2673e13f56dd9c7
Gonzalo España <gespana@alumnos.uai.cl>
2023-02-22 16:15:23 -0300

    variables de entorno añadidas

--- back/core/core/settings.py
+++ back/core/core/settings.py
@@ -10,6 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+# importar variables de entorno
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
