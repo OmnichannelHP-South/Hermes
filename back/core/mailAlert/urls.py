34e3d7b1488988597e2e6e68aa655bec93e870b5
Gonzalo España <gespana@alumnos.uai.cl>
2023-02-27 10:56:12 -0300

    restructuración del proyecto

--- back/core/mailAlert/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from .api import MailAlertViewSet
-
-router = routers.DefaultRouter()
-
-router.register('api/mailAlert', MailAlertViewSet, 'mailAlert')
-
-urlpatterns = router.urls
